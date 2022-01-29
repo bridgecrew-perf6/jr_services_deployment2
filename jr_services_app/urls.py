99cc619bb69bbb78ec5199d6f7905b859f72b586
jerenteria <renteria.juan96@gmail.com>
2022-01-29 13:41:44 -0800

    initial commit

---
+++ jr_services_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.index),
+    path('create_user', views.register), # matches "create_user" action in index.html "register" is function name in views
+    path('home', views.success),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('aboutus', views.aboutus),
+    path('make_payment', views.makePayment),
+]
